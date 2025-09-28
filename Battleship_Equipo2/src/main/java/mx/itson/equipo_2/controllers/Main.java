ee7aa7a8d730b507aca7d4cd69d08d0291af987b
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-09-27 17:28:38 -0700

    Prueba controller(despues se borraran)

---
+++ Battleship_Equipo2/src/main/java/mx/itson/equipo_2/controllers/Main.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mx.itson.equipo_2.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import mx.itson.equipo_2.models.PartidaModel;
+import mx.itson.equipo_2.models.entitys.Coordenada;
+import mx.itson.equipo_2.models.entitys.Jugador;
+import mx.itson.equipo_2.models.entitys.Nave;
+import mx.itson.equipo_2.models.enums.OrientacionNave;
+import mx.itson.equipo_2.models.enums.ResultadoDisparo;
+import mx.itson.equipo_2.models.enums.TipoNave;
+
+/**
+ *
+ * @author Jose Eduardo Aguilar Garcia
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+  
+        Jugador jugador1 = new Jugador("Jugador 1");
+        Jugador jugador2 = new Jugador("Jugador 2");
+
+        PartidaModel partidaModel = new PartidaModel(jugador1, jugador2);
+        PartidaController controller = new PartidaController(partidaModel);
+
+    
+        List<Coordenada> coordsNave = new ArrayList<>();
+        coordsNave.add(new Coordenada(0, 0));
+        coordsNave.add(new Coordenada(0, 1));
+        Nave nave = new Nave(TipoNave.CRUCERO, coordsNave, OrientacionNave.HORIZONTAL);
+        jugador2.getTablero().agregarNave(nave);
+
+       
+        System.out.println("Turno inicial: " + partidaModel.verificarTurno(jugador1));
+
+  
+        ResultadoDisparo r1 = controller.realizarDisparo(jugador1, new Coordenada(0, 0));
+        System.out.println("Jugador 1 dispara a (0,0): " + r1);
+
+    
+        ResultadoDisparo r2 = controller.realizarDisparo(jugador1, new Coordenada(0, 1));
+        System.out.println("Jugador 1 dispara a (0,1): " + r2);
+
+
+        if (partidaModel.partidaFinalizada()) {
+            System.out.println("La partida ha terminado.");
+        } else {
+            System.out.println("La partida sigue en curso.");
+        }
+
+    
+        try {
+            ResultadoDisparo r3 = controller.realizarDisparo(jugador2, new Coordenada(5, 5));
+            System.out.println("Jugador 2 dispara a (5,5): " + r3);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
+    
+    
