ab0349886c5ae52b15b22d7e726380f314896650
Jose Luis Islas <jose.islas252574@potros.itson.edu.mx>
2025-11-05 22:44:07 -0600

    Initialize Battleship project structure

---
+++ battleship_broker/src/main/java/com/itson/equipo2/battleship_broker/Battleship_broker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.equipo2.battleship_broker;
+
+/**
+ *
+ * @author skyro
+ */
+public class Battleship_broker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ battleship_cliente/src/main/java/com/itson/equipo2/battleship_cliente/Battleship_cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.equipo2.battleship_cliente;
+
+/**
+ *
+ * @author skyro
+ */
+public class Battleship_cliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
