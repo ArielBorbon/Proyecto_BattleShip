3c646c81c9d0759ba3ac6f2503a8eb312979d105
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-11-06 21:59:11 -0700

    cliente project estructura

---
+++ battleship_cliente/src/main/java/com/itson/equipo2/battleship_cliente/models/GameState.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itson.equipo2.battleship_cliente.models;
+
+/**
+ *
+ * @author Cricri
+ */
+public class GameState {
+    
+}
